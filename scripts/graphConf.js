87ca76bec0fc02a4ea87bcee204500c56eec615f
Wajih Ul Hassan <wajih.hassan@corelight.com>
2020-07-22 12:11:49 -0500

    first commit and changing defaults

--- scripts/graphConf.js
+++ scripts/graphConf.js
@@ -13,19 +13,19 @@ const SINGLE_COMMANDS_AND_NO_VARS = false;
 const REST_USE_HTTPS = false;
 
 // Time out for the REST protocol. Increase it if the graphDB is slow.
-const REST_TIMEOUT = 2000 
+const REST_TIMEOUT = 2000;
 // TODO: configuration for the secure server
 
 // limit number of nodes and edges to query for graph info
 // (avoid overwhelming the server for large graphs)
-const limit_graphinfo_request = 10000
+const limit_graphinfo_request = 10000;
 
 // Graph configuration
 const default_nb_of_layers = 3;
-const node_limit_per_request = 50;
+const node_limit_per_request = 30;
 
 // Simulation
-const force_strength = -600;
+const force_strength = -800;
 const link_strength = 0.2;
 const force_x_strength = 0.1;
 const force_y_strength = 0.1;
@@ -38,8 +38,8 @@ const active_node_margin = 6;
 const active_node_margin_opacity = 0.3;
 // Node position info in the DB
 // Replace by the key storing the node position inside the DB if any
-const node_position_x = 'graphexpx'
-const node_position_y = 'graphexpy'
+const node_position_x = 'graphexpx';
+const node_position_y = 'graphexpy';
 
 // Edges
 const default_edge_stroke_width = 3;
