22d8609453cfb17fd54bfaf5c47eeb03b9ab12d9
Tinoooo579 <113713203+Tinoooo579@users.noreply.github.com>
2023-10-21 20:35:59 -0400

    Tried to change html stuff

---
+++ scripts/graphIORemake.js
@@ -0,0 +1,48 @@
+import { Client } from '@elastic/elasticsearch';
+const clientURL = 'http://elastic:stimulus5affect-roof@beryl.cs.virginia.edu:9200'
+const client = new Client({
+    node: clientURL,
+});
+var graphio2 = (function () {
+
+
+// Define your Elasticsearch query
+
+    function searchQuery() {
+        let vertexValue =  "XcLR0ooBBBvBsP_nxiN9"//$('#search_field').val(); //Vertex Value given only support ids
+        let vertexKey = $('#search_value').val(); //Vertex Key
+        let vertexLabel = $('#label_field').val(); //Vertex Label Filter
+        const exectutedQuery = {
+            index: 'atlasv2-edr-h1-s4',
+            body: {
+                query: {
+                    match: {
+                        // Your query criteria
+                        _id: vertexValue
+                    }
+                }
+            }
+        };
+        runQuery(exectutedQuery)
+    }
+
+// Execute the query
+    async function runQuery(query) {
+        try {
+            const response = await client.search(query);
+            console.log(response.hits.hits); // currently only printing the query
+        } catch (error) {
+            console.error(`Error executing Elasticsearch query: ${error}`);
+        }
+    }
+    return {search_query: search_query}
+})()
+
+// Call the query function
+
+
+
+
+
+
+
